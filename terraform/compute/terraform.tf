2465c9b9c7907d41d5576aa4365f18537bbd071a
Spracto <barclayiversen@gmail.com>
2020-08-02 00:51:51 -0700

    Track app instance state separately

--- terraform/compute/terraform.tf
+++ terraform/compute/terraform.tf
@@ -7,6 +7,15 @@ data "terraform_remote_state" "foundation" {
     }
 }
 
+data "terraform_remote_state" "appip" {
+     backend = "gcs"
+    
+    config = {
+        bucket = "mixfreely"
+        prefix = "appip/"
+    }
+}
+
 terraform {
     required_version = "0.12.29"
 
